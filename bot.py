abaf941bab6526094702301b317c08c3361275d1
Chloe Horridge <nickh95@live.co.uk>
2018-08-23 00:19:50 +0100

    STPID DUMB 2.0

--- bot.py
+++ bot.py
@@ -83,8 +83,6 @@ async def on_ready():
 
     print('ready')
 
-    await client.change_presence(game=discord.Game(name="initializing..."))
-
 @client.event
 async def on_message(message):
     response = None
